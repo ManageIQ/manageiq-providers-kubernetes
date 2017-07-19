d519de20e0a9ac0e8cab0bf76aa6a642e8af95ed
Beni Cherniavsky-Paskin <cben@redhat.com>
2017-07-19 13:04:07 +0300

    Unhide ems_refresh.kubernetes.inventory_object_refresh setting

--- app/models/manageiq/providers/kubernetes/container_manager/refresher.rb
+++ app/models/manageiq/providers/kubernetes/container_manager/refresher.rb
@@ -7,7 +7,7 @@ module ManageIQ::Providers::Kubernetes
       entities = ems.with_provider_connection { |client| fetch_entities(client, KUBERNETES_ENTITIES) }
       EmsRefresh.log_inv_debug_trace(entities, "inv_hash:")
 
-      if refresher_options.try(:[], :inventory_object_refresh)
+      if refresher_options.inventory_object_refresh
         ManageIQ::Providers::Kubernetes::ContainerManager::RefreshParser.ems_inv_to_inv_collections(ems, entities, refresher_options)
       else
         ManageIQ::Providers::Kubernetes::ContainerManager::RefreshParser.ems_inv_to_hashes(entities, refresher_options)

bf2c1c01398d60c6a2aa681660aa44c3c46f7423
Adam Grare <agrare@redhat.com>
2017-10-12 16:15:56 -0400

    Parse pod ems_ref and queue targeted refresh

--- app/models/manageiq/providers/kubernetes/container_manager/inventory_collector_worker/runner.rb
+++ app/models/manageiq/providers/kubernetes/container_manager/inventory_collector_worker/runner.rb
@@ -10,6 +10,20 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::InventoryCollectorWorke
   def do_work
     pod_watch_stream.each do |notice|
       _log.info("EMS [#{ems.id}] Pod: #{notice}")
+
+      ems_ref = parse_notice_pod_ems_ref(notice.object)
+
+      target = ManagerRefresh::Target.new(
+        :manager     => ems,
+        :association => :container_groups,
+        :manager_ref => ems_ref,
+        :options     => {
+          :payload => notice.object,
+        },
+      )
+
+      EmsRefresh.queue_refresh(target)
+
       heartbeat
     end
   end
@@ -18,5 +32,9 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::InventoryCollectorWorke
 
   def connection
     @connection ||= ems.connect(:service => "kubernetes", :path => '/api')
+  end
+
+  def parse_notice_pod_ems_ref(pod)
+    pod.metadata.uid
   end
 end

c16eb0d41549bd25e69b306dd1b32398f4fb29a7
Moti Asayag <masayag@redhat.com>
2017-12-18 12:31:27 +0200

    Support KubeVirt provider
    The kubevirt provider will be managed as a virtualization manager under
    the container managers.
    
    The endpoint and authentication of kubevirt will be selected by
    :kubevirt role and authkey.

---
+++ app/models/manageiq/providers/kubernetes/virtualization_manager_mixin.rb
@@ -0,0 +1,32 @@
+module ManageIQ::Providers::Kubernetes::VirtualizationManagerMixin
+  extend ActiveSupport::Concern
+
+  ENDPOINT_ROLE = :kubevirt
+
+  included do
+    delegate :authentication_check,
+             :authentication_for_summary,
+             :authentication_status,
+             :authentication_status_ok,
+             :authentication_token,
+             :authentications,
+             :endpoints,
+             :zone,
+             :to        => :parent_manager,
+             :allow_nil => true
+  end
+
+  module ClassMethods
+    def raw_connect(options)
+      ManageIQ::Providers::Kubevirt::InfraManager.raw_connect(options)
+    end
+  end
+
+  def virtualization_endpoint
+    connection_configurations.kubevirt.try(:endpoint)
+  end
+
+  def default_authentication_type
+    ENDPOINT_ROLE
+  end
+end

5c386c4ef8a0406e5c6d3678f4d229230eb86a82
Beni Cherniavsky-Paskin <cben@redhat.com>
2018-02-20 22:04:41 +0200

    Test :batch strategy with and without :use_ar_object

--- spec/models/manageiq/providers/kubernetes/container_manager/targeted_refresh/targeted_refresh_spec.rb
+++ spec/models/manageiq/providers/kubernetes/container_manager/targeted_refresh/targeted_refresh_spec.rb
@@ -421,24 +421,20 @@ describe ManageIQ::Providers::Kubernetes::ContainerManager::Refresher do
       expect(ManageIQ::Providers::Kubernetes::ContainerManager::RefreshParser).not_to receive(:ems_inv_to_hashes)
     end
 
-    context "with :default saver" do
-      before(:each) do
-        stub_settings_merge(
-          :ems_refresh => {:kubernetes => {:inventory_collections => {:saver_strategy => :default}}}
-        )
+    [
+      {:saver_strategy => :default},
+      {:saver_strategy => :batch, :use_ar_object => true},
+      {:saver_strategy => :batch, :use_ar_object => false}
+    ].each do |saver_options|
+      context "with #{saver_options}" do
+        before(:each) do
+          stub_settings_merge(
+            :ems_refresh => {:kubernetes => {:inventory_collections => saver_options}}
+          )
+        end
+
+        include_examples "openshift refresher VCR targeted refresh tests"
       end
-
-      include_examples "openshift refresher VCR targeted refresh tests"
-    end
-
-    context "with :batch saver" do
-      before(:each) do
-        stub_settings_merge(
-          :ems_refresh => {:kubernetes => {:inventory_collections => {:saver_strategy => :batch}}}
-        )
-      end
-
-      include_examples "openshift refresher VCR targeted refresh tests"
     end
   end
 end

97f73de7045fc5219b0af13339b8cd90b76a6b01
Ladislav Smola <lsmola@redhat.com>
2018-08-11 20:58:22 +0200

    We can use targeted => true
    We can use targeted => true and targeted specs are still passing

--- app/models/manageiq/providers/kubernetes/inventory/persister/target_collection.rb
+++ app/models/manageiq/providers/kubernetes/inventory/persister/target_collection.rb
@@ -1,15 +1,9 @@
 class ManageIQ::Providers::Kubernetes::Inventory::Persister::TargetCollection < ManageIQ::Providers::Kubernetes::Inventory::Persister::ContainerManager
   def targeted?
-    false # TODO(lsmola) get ready for true, which means a proper targeted refresh. That will require more effort.
+    true
   end
 
-  def shared_options
-    {
-      :targeted => targeted?,
-      :complete => false, # For now, we want to a only create and update elements using watches data, delete events could
-      # probably set finished_at and deleted_on dates, as an update based disconnect_inv.
-      :strategy => :local_db_find_missing_references, # By default no IC will be saved
-      :parent   => manager.presence
-    }
+  def strategy
+    :local_db_find_missing_references
   end
 end

adebdbb6c1e3351a0ca967e6e9b1d94386690500
Ladislav Smola <lsmola@redhat.com>
2018-08-27 11:46:11 +0200

    Add TODO for iterator, so we can properly add it later
    Add TODO for iterator, so we can properly add it later

--- app/models/manageiq/providers/kubernetes/inventory/collector/container_manager.rb
+++ app/models/manageiq/providers/kubernetes/inventory/collector/container_manager.rb
@@ -1,22 +1,32 @@
 class ManageIQ::Providers::Kubernetes::Inventory::Collector::ContainerManager < ManageIQ::Providers::Kubernetes::Inventory::Collector
+  # TODO(lsmola) we need to return iterator for each collection, so we avoid fetching too many items to memory at
+  # once.
+  # Hints from cben:
+  # A slightly open question in kubeclient iterator interface was how it should expose whole-collection resource_version
+  # (and other metadata). Should iterator yield it with every item, or return it at the end?
+  # With streaming parse (abonas/kubeclient#254), this depends on order of json. k8s puts items last, so version is
+  # known by time we start yielding; this is unlikely to change but I feel weird hardcoding this assumption...
+  # For chunking (abonas/kubeclient#283), we'll have metadata in each chunk, so either API works. Chunking also brings
+  # risk of getting 410 Gone if we wait too long, not sure how to handle that.
+
   def namespaces
-    @namespaces ||= entities_iterator(connection, :namespaces)
+    @namespaces ||= connection.get_namespaces
   end
 
   def pods
-    @pods ||= entities_iterator(connection, :pods)
+    @pods ||= connection.get_pods
   end
 
   def cluster_service_offerings
-    @cluster_service_offerings ||= entities_iterator(service_catalog_connection, :cluster_service_classes)
+    @cluster_service_offerings ||= service_catalog_connection.get_cluster_service_classes
   end
 
   def cluster_service_parameters_sets
-    @cluster_service_parameters_sets ||= entities_iterator(service_catalog_connection, :cluster_service_plans)
+    @cluster_service_parameters_sets ||= service_catalog_connection.get_cluster_service_plans
   end
 
   def service_instances
-    @service_instances ||= entities_iterator(service_catalog_connection, :service_instances)
+    @service_instances ||= service_catalog_connection.get_service_instances
   end
 
   private
@@ -27,16 +37,5 @@ class ManageIQ::Providers::Kubernetes::Inventory::Collector::ContainerManager < 
 
   def service_catalog_connection
     @service_catalog_connection ||= manager.connect(:service => "kubernetes_service_catalog")
-  end
-
-  def entities_iterator(client, entity)
-    # TODO(lsmola) change to iterator, that will fetch paginated response from the server, never fetching everything
-    # at once
-    client.send("get_#{entity}")
-  rescue KubeException => e
-    # TODO(lsmola) the old refresh has entities that can throws error and return some :default set
-    _log.error("Unexpected Exception during fetching of entity #{entity}: #{e}")
-    _log.error(e.backtrace.join("\n"))
-    []
   end
 end

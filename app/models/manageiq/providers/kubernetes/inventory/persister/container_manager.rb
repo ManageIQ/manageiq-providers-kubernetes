14cfbf7fa0218700a73b13df4f1240f812704f88
Adam Grare <agrare@redhat.com>
2019-01-04 15:24:19 -0500

    Remove duplicated persister shared_options
    The shared_options were duplicated from the base persister

--- app/models/manageiq/providers/kubernetes/inventory/persister/container_manager.rb
+++ app/models/manageiq/providers/kubernetes/inventory/persister/container_manager.rb
@@ -4,22 +4,4 @@ class ManageIQ::Providers::Kubernetes::Inventory::Persister::ContainerManager < 
   def initialize_inventory_collections
     initialize_container_inventory_collections
   end
-
-  protected
-
-  def targeted?
-    false
-  end
-
-  def strategy
-    nil
-  end
-
-  def shared_options
-    {
-      :strategy => strategy,
-      :targeted => targeted?,
-      :parent   => manager.presence
-    }
-  end
 end

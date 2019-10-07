cf284e867ff7d3fcacec4c0eb35d4e5cf58d5ddc
Adam Grare <agrare@redhat.com>
2019-10-07 13:12:52 -0400

    Add params_for_create and verify_credentials
    Add methods to get the required parameters to create a provider and a
    method to verify the credentials.

--- app/models/manageiq/providers/kubernetes/container_manager.rb
+++ app/models/manageiq/providers/kubernetes/container_manager.rb
@@ -49,6 +49,89 @@ class ManageIQ::Providers::Kubernetes::ContainerManager < ManageIQ::Providers::C
     @description ||= "Kubernetes".freeze
   end
 
+  def self.params_for_create
+    @params_for_create ||= {
+      :title  => "Configure #{description}",
+      :fields => [
+        {
+          :component  => "text-field",
+          :name       => "endpoints.default.hostname",
+          :label      => "Hostname",
+          :isRequired => true,
+          :validate   => [{:type => "required-validator"}]
+        },
+        {
+          :component  => "text-field",
+          :name       => "endpoints.default.port",
+          :type       => "number",
+          :isRequired => true,
+          :validate   => [
+            {
+              :type => "required-validator"
+            },
+            {
+              :type             => "validatorTypes.MIN_NUMBER_VALUE",
+              :includeThreshold => true,
+              :value            => 1
+            },
+            {
+              :type             => "validatorTypes.MAX_NUMBER_VALUE",
+              :includeThreshold => true,
+              :value            => 65_535
+            }
+          ]
+        },
+        {
+          :component    => "text-field",
+          :name         => "endpoints.default.path",
+          :label        => "API Path",
+          :type         => "hidden",
+          :initialValue => "/api"
+        },
+        {
+          :component  => "text-field",
+          :name       => "endpoints.default.bearer",
+          :label      => "Token",
+          :type       => "password",
+          :isRequired => true,
+          :validate   => [{:type => "required-validator"}]
+        },
+        {
+          :component => "text-field",
+          :name      => "endpoints.default.http_proxy",
+          :label     => "HTTP Proxy"
+        },
+        {
+          :component => "checkbox",
+          :name      => "endpoints.default.verify_ssl",
+          :label     => "Verify SSL"
+        },
+        {
+          :component => "text-field",
+          :name      => "endpoints.default.ca_file",
+          :label     => "Trusted CA Certificates",
+        },
+      ]
+    }.freeze
+  end
+
+  def self.verify_credentials(args)
+    default_endpoint = args.dig("endpoints", "default")
+    hostname, port = default_endpoint&.values_at("hostname", "port")
+
+    options = default_endpoint&.slice("path", "http_proxy")&.symbolize_keys || {}
+
+    bearer = default_endpoint&.dig("bearer")
+    options[:bearer] = MiqPassword.try_decrypt(bearer) if bearer
+
+    options[:ssl_options] = {
+      :verify_ssl => default_endpoint&.dig("verify_ssl") ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE,
+      :ca_file    => default_endpoint&.dig("ca_file")
+    }
+
+    !!raw_connect(hostname, port, options)
+  end
+
   def self.raw_connect(hostname, port, options)
     kubernetes_connect(hostname, port, options)
   end

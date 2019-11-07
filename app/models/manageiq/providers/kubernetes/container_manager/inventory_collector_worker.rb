4d9bc9274251c0836c1eb7edf4ff887eac73b80e
Adam Grare <agrare@redhat.com>
2019-11-07 09:21:44 -0500

    Remove unused InventoryCollectorWorker
    The InventoryCollectorWorker was an experimental streaming refresh
    worker for kubernetes/openshift but was never used.

--- app/models/manageiq/providers/kubernetes/container_manager/inventory_collector_worker.rb
+++
@@ -1,8 +0,0 @@
-class ManageIQ::Providers::Kubernetes::ContainerManager::InventoryCollectorWorker < ManageIQ::Providers::BaseManager::InventoryCollectorWorker
-  require_nested :Runner
-
-  def self.has_required_role?
-    return false unless Settings.fetch_path(:ems_refresh, ems_class.ems_type.to_sym, :inventory_object_refresh)
-    super
-  end
-end

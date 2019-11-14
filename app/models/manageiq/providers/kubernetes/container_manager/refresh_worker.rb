d0c8d85baf8f6fbbf254b6f8997700c997634eb1
Nick Carboni <ncarboni@redhat.com>
2019-11-14 16:25:46 -0500

    Rely on the default implementation of .ems_class

--- app/models/manageiq/providers/kubernetes/container_manager/refresh_worker.rb
+++ app/models/manageiq/providers/kubernetes/container_manager/refresh_worker.rb
@@ -1,6 +1,3 @@
 class ManageIQ::Providers::Kubernetes::ContainerManager::RefreshWorker < ManageIQ::Providers::BaseManager::RefreshWorker
   require_nested :Runner
-  def self.ems_class
-    ManageIQ::Providers::Kubernetes::ContainerManager
-  end
 end

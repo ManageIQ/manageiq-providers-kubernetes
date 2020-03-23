be65692b57e561acab4e2507c808374f8b702662
Gregg Tanzillo <gtanzill@redhat.com>
2020-03-23 15:24:43 -0400

    Merge pull request #360 from agrare/cleanup_kubernetes_watches_refresh
    Cleanup watches refresh from topology
    
    (cherry picked from commit 0f825b8fac4cb5c4c2a611a9da572c4ed576898e)

--- app/models/manageiq/providers/kubernetes/container_manager/refresh_worker/runner.rb
+++ app/models/manageiq/providers/kubernetes/container_manager/refresh_worker/runner.rb
@@ -1,3 +1,2 @@
 class ManageIQ::Providers::Kubernetes::ContainerManager::RefreshWorker::Runner < ManageIQ::Providers::BaseManager::RefreshWorker::Runner
-  include ManageIQ::Providers::Kubernetes::ContainerManager::StreamingRefreshMixin
 end

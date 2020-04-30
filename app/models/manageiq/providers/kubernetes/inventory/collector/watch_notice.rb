e0d6ecc1daecaeb9d6c7983454713cddb3744aa7
Adam Grare <agrare@redhat.com>
2020-04-30 10:10:38 -0400

    Parse notices separate from objects, use normal parser

--- app/models/manageiq/providers/kubernetes/inventory/collector/watch_notice.rb
+++ app/models/manageiq/providers/kubernetes/inventory/collector/watch_notice.rb
@@ -1,10 +1,13 @@
 class ManageIQ::Providers::Kubernetes::Inventory::Collector::WatchNotice < ManageIQ::Providers::Kubernetes::Inventory::Collector
   attr_reader :additional_attributes, :pods, :services, :endpoints, :replication_controllers,
-              :nodes, :namespaces, :resource_quotas, :limit_ranges, :persistent_volumes, :persistent_volume_claims
+              :namespaces, :nodes, :notices, :resource_quotas, :limit_ranges,
+              :persistent_volumes, :persistent_volume_claims
 
   def initialize(manager, notices)
+    @notices = filter_notices(notices)
+
     initialize_collections!
-    parse_notices!(notices)
+    populate_collections!
 
     super(manager, nil)
   end
@@ -12,28 +15,37 @@ class ManageIQ::Providers::Kubernetes::Inventory::Collector::WatchNotice < Manag
   private
 
   def initialize_collections!
-    @additional_attributes = {}
-    @pods = []
-    @services = []
-    @endpoints = []
-    @replication_controllers = []
-    @nodes = []
-    @namespaces = []
-    @resource_quotas = []
-    @limit_ranges = []
-    @persistent_volumes = []
+    @additional_attributes    = {}
+    @pods                     = []
+    @services                 = []
+    @endpoints                = []
+    @replication_controllers  = []
+    @nodes                    = []
+    @namespaces               = []
+    @resource_quotas          = []
+    @limit_ranges             = []
+    @persistent_volumes       = []
     @persistent_volume_claims = []
   end
 
-  def parse_notices!(all_notices)
+  # The notices returned by the Kubernetes API contain always the complete
+  # representation of the object, so it isn't necessary to process all of them,
+  # only the last one for each object.
+  def filter_notices(all_notices)
     notices_by_kind = all_notices.group_by { |notice| notice.object&.kind }.except(nil)
-    notices_by_kind.each do |kind, notices|
-      # The notices returned by the Kubernetes API contain always the complete representation of the object, so it isn't
-      # necessary to process all of them, only the last one for each object.
-      notices.reverse!.uniq! { |n| n.object&.metadata&.uid }
 
-      # Only add ADDED/MODIFIED to the collectors so deleted objects will be removed
-      instance_variable_get("@#{kind.tableize}")&.concat(notices)
+    notices_by_kind.values.each_with_object([]) do |notices, result|
+      notices.reverse!.uniq! { |n| n.object&.metadata&.uid }
+      result.concat(notices)
+    end
+  end
+
+  # Pull the object out of the notices and populate the normal collections
+  # so that the Parser::ContainerManager can be used normally
+  def populate_collections!
+    # Only add ADDED/MODIFIED to the collectors so deleted objects will be removed
+    notices.reject { |n| n.type == "DELETED" }.each do |notice|
+      instance_variable_get("@#{notice.object.kind.tableize}") << notice.object
     end
   end
 end

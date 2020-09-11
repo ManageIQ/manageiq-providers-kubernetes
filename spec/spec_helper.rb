68f212834f5f6c56ac4255cdc96895e12437a94f
Jason Frey <fryguy9@gmail.com>
2020-09-11 16:12:49 -0400

    Plugin Generator Updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,13 @@ if ENV['CI']
   SimpleCov.start
 end
 
+Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
+# NOTE: This repo's spec/support/ is also used by manageiq-providers-openshift.
+Dir[File.join(__dir__, "support/**/*.rb")].each { |f| require f }
+
+require "manageiq-providers-kubernetes"
+
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::Kubernetes::Engine.root, 'spec/vcr_cassettes')
 end
-
-Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
-# This repo's spec/support/ is also used by manageiq-providers-openshift.
-Dir[ManageIQ::Providers::Kubernetes::Engine.root.join("spec/support/**/*.rb")].each { |f| require f }

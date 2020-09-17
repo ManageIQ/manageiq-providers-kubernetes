fa092917f74255c8499e02e79236ed418e08e757
Adam Grare <agrare@redhat.com>
2020-09-17 08:05:47 -0400

    Use the tag_mapper from the core Persister

--- app/models/manageiq/providers/kubernetes/inventory/persister.rb
+++ app/models/manageiq/providers/kubernetes/inventory/persister.rb
@@ -1,8 +1,4 @@
 class ManageIQ::Providers::Kubernetes::Inventory::Persister < ManageIQ::Providers::Inventory::Persister
   require_nested :ContainerManager
   require_nested :WatchNotice
-
-  def add_collection_directly(collection)
-    @collections[collection.name] = collection
-  end
 end

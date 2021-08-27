ff65fab93be875016aa958c584e5a6332e8a08c3
Adam Grare <adam@grare.com>
2021-08-27 10:29:39 -0400

    Add specs for 410 Gone and 401 Unauthorized

--- spec/models/manageiq/providers/kubernetes/container_manager/refresh_worker/watch_thread_spec.rb
+++ spec/models/manageiq/providers/kubernetes/container_manager/refresh_worker/watch_thread_spec.rb
@@ -3,7 +3,7 @@ describe ManageIQ::Providers::Kubernetes::ContainerManager::RefreshWorker::Watch
 
   let(:ems)              { FactoryBot.create(:ems_kubernetes) }
   let(:queue)            { Queue.new }
-  let(:resource_version) { nil }
+  let(:resource_version) { "1" }
   let(:entity_type)      { nil }
   let(:notice)           { Kubeclient::Resource.new(:type => "MODIFIED", :object => object) }
   let(:watch_thread)     { described_class.new({}, ems.class, queue, entity_type, resource_version) }
@@ -15,15 +15,15 @@ describe ManageIQ::Providers::Kubernetes::ContainerManager::RefreshWorker::Watch
     let(:object)      { Kubeclient::Resource.new(:kind => "Pod", :metadata => {:name => "pod-696ddc596b-qs64c", :namespace => "default", :resourceVersion => "2"}) }
 
     before do
-      expect(watch_thread).to receive(:running?).and_return(true, false)
-      expect(watch_thread).to receive(:connection).with(entity_type).and_return(connection)
+      allow(watch_thread).to receive(:running?).and_return(true, false)
+      allow(watch_thread).to receive(:connection).with(entity_type).and_return(connection)
 
-      expect(connection)
+      allow(connection)
         .to receive(:send)
         .with("watch_#{entity_type}", :resource_version => resource_version)
         .and_return(watch)
 
-      expect(watch).to receive(:each).and_yield(notice).once
+      allow(watch).to receive(:each).and_yield(notice).once
     end
 
     it "queues a notice" do
@@ -31,6 +31,41 @@ describe ManageIQ::Providers::Kubernetes::ContainerManager::RefreshWorker::Watch
 
       expect(queue.length).to eq(1)
       expect(queue.pop).to    eq(notice)
+    end
+
+    it "updates the last resource_version" do
+      expect(watch_thread).to receive(:resource_version=).with("2")
+      expect(watch_thread).to receive(:resource_version=).with(nil) # This is set when the watch loop breaks
+
+      watch_thread.send(:collector_thread)
+    end
+
+    context "410 Gone" do
+      let(:object) { Kubeclient::Resource.new(:kind => "Status", :code => 410, :reason => "Gone") }
+      let(:notice) { Kubeclient::Resource.new(:type => "ERROR", :object => object) }
+
+      it "restarts the watch from the start" do
+        expect(watch_thread).to receive(:resource_version=).with(nil)
+
+        watch_thread.send(:collector_thread)
+      end
+    end
+
+    context "401 Unauthorized" do
+      before do
+        allow(watch_thread).to receive(:running?).and_return(true, true, false)
+        expect(watch).to receive(:each).and_raise(Kubeclient::HttpError.new(401, "Unauthorized", "Unauthorized"))
+      end
+
+      it "restarts the watch from the last resource_version" do
+        expect(connection)
+          .to receive(:send)
+          .twice
+          .with("watch_#{entity_type}", :resource_version => resource_version)
+          .and_return(watch)
+
+        watch_thread.send(:collector_thread)
+      end
     end
   end
 

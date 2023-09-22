8c101b1bdfbba930ea3a80cf3dbabddd4cae4b48
Adam Grare <adam@grare.com>
2023-09-22 13:14:35 -0400

    Remove unused MonitoringManager

--- app/models/manageiq/providers/kubernetes/monitoring_manager/event_catcher.rb
+++
@@ -1,9 +0,0 @@
-class ManageIQ::Providers::Kubernetes::MonitoringManager::EventCatcher < ManageIQ::Providers::BaseManager::EventCatcher
-  require_nested :Runner
-  require_nested :RunnerMixin
-  require_nested :Stream
-
-  def self.settings_name
-    :event_catcher_prometheus
-  end
-end

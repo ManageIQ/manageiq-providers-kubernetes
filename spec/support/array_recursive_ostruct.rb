dfd0eae9986364f2303e458f47f17bd180887b14
Jason Frey <fryguy9@gmail.com>
2025-05-12 14:05:40 -0400

    Merge pull request #550 from agrare/fix_private_kubernetes_client_spec_failures
    Fix Scanning Job spec failure private method
    
    (cherry picked from commit 9e23929afa67ad119a7acc011eaf2d87ff60bbc0)

--- spec/support/array_recursive_ostruct.rb
+++ spec/support/array_recursive_ostruct.rb
@@ -1,5 +1,7 @@
 # Helps constructing inputs similar to kubeclient results
 module ArrayRecursiveOpenStruct
+  require 'recursive-open-struct'
+
   def array_recursive_ostruct(hash)
     RecursiveOpenStruct.new(hash, :recurse_over_arrays => true)
   end

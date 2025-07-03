41e2b18255c8e05539981d7cdec6dd6a3349c128
Adam Grare <adam@grare.com>
2025-07-03 09:51:08 -0400

    Fix whitespace and unused argument

--- app/models/manageiq/providers/kubernetes/container_manager/container_project.rb
+++ app/models/manageiq/providers/kubernetes/container_manager/container_project.rb
@@ -9,11 +9,11 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::ContainerProject < ::Co
   def self.raw_create_container_project(ext_management_system, options)
     project_name = options["name"]
     raise ArgumentError, _("Must specify a name for the container project") if project_name.blank?
-    
+
     unless project_name.match?(/\A[a-z0-9]([-a-z0-9]*[a-z0-9])?\z/)
       raise ArgumentError, _("Name must consist of lower case alphanumeric characters or '-', start with an alphanumeric character, and end with an alphanumeric character")
     end
-    
+
     if project_name.length > 63
       raise ArgumentError, _("Name must be no more than 63 characters")
     end
@@ -33,7 +33,7 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::ContainerProject < ::Co
 
     core_client = ext_management_system.connect(:service => :kubernetes)
     result = core_client.create_namespace(namespace_payload)
-    
+ 
     {:ems_ref => result.metadata.name, :name => result.metadata.name}
   rescue Kubeclient::HttpError => e
     if e.error_code == 409
@@ -47,10 +47,10 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::ContainerProject < ::Co
 
   def raw_delete_container_project
     connection = ext_management_system.connect(:service => :kubernetes)
-    
+
     begin
       connection.delete_namespace(name)
-    rescue Kubeclient::ResourceNotFoundError => e
+    rescue Kubeclient::ResourceNotFoundError
       # Namespace already deleted, no action needed
     rescue Kubeclient::HttpError => e
       raise MiqException::Error, "Kubernetes API error: #{e.message}"

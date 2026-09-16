09c9075109dc52e8d887b3f119162e4803babdf5
ohhAndy <andy.hujze@gmail.com>
2026-09-16 12:01:25 -0400

    Configure EventCatcher to support standalone worker mode
    - Add rails_worker lambda and worker_settings_paths to event_catcher.rb
    - Override worker_options in runner.rb to decrypt credentials before JSON serialisation
    - Add :rails_worker: false setting under event_catcher_kubernetes

--- app/models/manageiq/providers/kubernetes/container_manager/event_catcher.rb
+++ app/models/manageiq/providers/kubernetes/container_manager/event_catcher.rb
@@ -1,2 +1,4 @@
 class ManageIQ::Providers::Kubernetes::ContainerManager::EventCatcher < ManageIQ::Providers::BaseManager::EventCatcher
+  self.rails_worker = -> { !!worker_settings[:rails_worker] }
+  self.worker_settings_paths = [%i[ems ems_kubernetes]]
 end
